c3929146f6369e669a6ba0032a39b52d44ba8b3a
Shubhampatil690 <690spatil@gmail.com>
2022-12-03 19:53:13 +0530

    Test

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule,NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas:[NO_ERRORS_SCHEMA,
+    CUSTOM_ELEMENTS_SCHEMA
+
+  ] 
+})
+export class AppModule { }
